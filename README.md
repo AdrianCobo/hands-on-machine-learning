5479b180faaf83da112abecf32d8e30e26fd93b8
adrianCo <adrian.cobo.mh@gmail.com>
2022-08-24 09:57:42 +0200

    apuntes tema 2
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hands-on-machine-learning
+
+In this repo you will find my notes about the book:"Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow" by Aurélien Géron
+
+Most of the programs and excercises are just copied from the book and the github repo about the book: https://github.com/ageron/handson-ml3.
---
+++ tema1/linearRegression.py
@@ -0,0 +1,43 @@
+# Python 3.7 is required:
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+import numpy as np
+import sys
+assert sys.version_info >= (3, 7)
+# Make this notebook's output stable across runs:
+np.random.seed(42)
+# Scikit-Learn ≥1.0 is required:
+assert sklearn.__version__ >= "1.0"
+
+# Let's define the default font sizes, to plot pretty figures:
+
+plt.rc('font', size=12)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=12)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+
+# Download and prepare the data
+data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+X = lifesat[["GDP per capita (USD)"]].values
+y = lifesat[["Life satisfaction"]].values
+
+# Visualize the data
+lifesat.plot(kind='scatter', grid=True,
+             x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+# Select a linear model
+model = LinearRegression()
+
+# Train the model
+model.fit(X, y)
+
+# Make a prediction for Cyprus
+X_new = [[37_655.2]]  # Cyprus' GDP per capita in 2020
+print(model.predict(X_new))  # outputs [[6.30165767]]
